529f8899c145319e3acbe397240ab10867005f2f
Md. Mehedi Hasan <mehedi53423@gmail.com>
2023-08-23 23:40:54 +0600

    DbContext Added

---
+++ FormulaOne.DataService/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using FormulaOne.Entities.DbSet;
+using Microsoft.EntityFrameworkCore;
+
+namespace FormulaOne.DataService.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public virtual DbSet<Driver> Drives { get; set; }
+        public virtual DbSet<Achievement> Achievements { get; set; }
+    }
+}
